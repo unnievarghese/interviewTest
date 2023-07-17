20fa3e66eac8944540ab3cae48f8de2aa052addc
sayone <you@example.com>
2023-07-17 11:49:36 +0530

    changes made to add product and sale objects from main method, as required.

--- Test/src/main/java/com/Ecom/Test/InitialUserSetup.java
+++ Test/src/main/java/com/Ecom/Test/InitialUserSetup.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
@@ -25,17 +26,23 @@ public class InitialUserSetup {
     @Autowired
     RoleRepository roleRepository;
 
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @EventListener
     @Transactional
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Roles roles = createRole("ROLE_ADMIN");
-        User user = userRepository.getUserByEmail(email);
-        if(user != null && user.getRoles().isEmpty()){
-            Collection<Roles> rolesCollection = new ArrayList<>();
-            rolesCollection.add(roles);
-            user.setRoles(rolesCollection);
-            userRepository.save(user);
-        }
+        User user = new User();
+        user.setUserName("unnie");
+        user.setEmail("admin@gmail.com");
+        user.setPassword(bCryptPasswordEncoder.encode("123"));
+        userRepository.save(user);
+        Collection<Roles> rolesCollection = new ArrayList<>();
+        rolesCollection.add(roles);
+        user.setRoles(rolesCollection);
+        userRepository.save(user);
+
     }
 
     public Roles createRole(String name){
--- Test/src/main/java/com/Ecom/Test/TestApplication.java
+++ Test/src/main/java/com/Ecom/Test/TestApplication.java
@@ -1,15 +1,51 @@
 package com.Ecom.Test;
 
+import com.Ecom.Test.Model.Product;
+import com.Ecom.Test.Model.Sale;
+import com.Ecom.Test.Repository.ProductRepository;
+import com.Ecom.Test.Repository.SaleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+
+		ProductRepository productRepository = (ProductRepository) SpringApplicationContext.getBean("productRepository");
+
+		Product product = new Product();
+		product.setName("galaxy s20");
+		product.setPrice(new BigDecimal("65000.75"));
+		product.setDescription("best android phone ever");
+		product.setQuantity(10);
+		productRepository.save(product);
+
+		Product product1 = new Product();
+		product1.setName("galaxy s10");
+		product1.setPrice(new BigDecimal("75000.65"));
+		product1.setDescription("best android phone.");
+		product1.setQuantity(10);
+		productRepository.save(product1);
+
+		SaleRepository saleRepository = (SaleRepository) SpringApplicationContext.getBean("saleRepository");
+
+		Sale sale = new Sale();
+		sale.setSaleDate(LocalDate.now());
+		sale.setQuantity(2);
+		sale.setProductDetails(product);
+		saleRepository.save(sale);
+
+		Sale sale1 = new Sale();
+		sale1.setProductDetails(product1);
+		sale1.setQuantity(2);
+		sale1.setSaleDate(LocalDate.now());
+		saleRepository.save(sale1);
 	}
 
 	@Bean
@@ -21,4 +57,6 @@ public class TestApplication {
 	public SpringApplicationContext springApplicationContext(){
 		return new SpringApplicationContext();
 	}
+
+
 }
